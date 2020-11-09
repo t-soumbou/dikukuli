1276591ad8be0e785e277b6a8ce90a3f8b74ee3a
t-soumbou <terrencesoumbou@gmail.com>
2020-11-09 15:48:47 +0100

    Update app_rest.py

--- app_rest.py
+++ app_rest.py
@@ -1,9 +1,1 @@
-# Web app entry point
 
-import bottle
-import os
-import app_rest_modules
-
-if __name__ == "__main__":
-    app_rest_modules.init()
-    bottle.run(port=int(os.environ.get("PORT", 5000)))
