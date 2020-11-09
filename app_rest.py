b4ab53da22fd482aaa5adf346453071dba8dc831
t-soumbou <terrencesoumbou@gmail.com>
2020-11-09 16:00:11 +0100

    Update app_rest.py

--- app_rest.py
+++ app_rest.py
@@ -6,5 +6,4 @@ import app_rest_modules
 
 if __name__ == "__main__":
     app_rest_modules.init()
-    port = int(os.environ.get('PORT', 33507))
-    bottle.run(port=port)
+    bottle.run(server='cgi')
